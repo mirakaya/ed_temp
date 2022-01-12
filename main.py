31a0567b3edd10109a296b2de10bf8c6bd4c1f9e
mirakaya <59024914+mirakaya@users.noreply.github.com>
2022-01-12 02:19:41 -0600

    ~93.8% accuracy

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 #numeric
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.feature_selection import RFE
+
+from sklearn.datasets import make_blobs
+
+
+import statsmodels.api as sm
+
 
 
 # evaluate random forest algorithm for classification
@@ -45,7 +55,7 @@ def readFile(path):
     aux = 0
     for i in df.columns:
 
-        if cor_target[aux] < 0.1 or cor_target[aux] >0.90: #if they are too different or too similar they dont carry info
+        if cor_target[aux] < 0.2 or cor_target[aux] >0.9: #if they are too different or too similar they dont carry info
             df.drop(i, axis=1, inplace=True)
         aux += 1
 
@@ -60,9 +70,9 @@ def readFile(path):
     df.drop('85', axis=1, inplace=True)
     df.drop('1999', axis=1, inplace=True)'''
 
-    #print(df)
+    #print(df.columns)
 
-    perc_info = 0.8
+    perc_info = 1
     n_info = int((df.shape[1] - 1) * perc_info)
     n_redu = int((df.shape[1] - 1) * (1-perc_info))
 
@@ -80,20 +90,79 @@ def readFile(path):
     model = RandomForestClassifier()
 
     # evaluate the model
-    cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+    cv = RepeatedStratifiedKFold(n_splits=15, n_repeats=5, random_state=3)
     n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
 
     # report performance
     print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
 
-    '''    # fit the model on the whole dataset
-    model.fit(X, y)
+    # fit the model on the whole dataset
+    #model.fit(X, y)
+    '''
     # make a single prediction
     row = [
         [0,0.64,0.64,0,0.32,0,0,0,0,0,0,0.64,0,0,0,0.32,0,1.29,1.93,0,0.96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.778,0,0,3.756,61,278
 ]]
     yhat = model.predict(row)
     print('Prediction: %d' % yhat[0])'''
+
+
+    #---------untested section neural networks
+
+    '''#n_samples= int(len(df.index) * 0.8), n_features=df.shape[1] -1, n_informative=n_info, n_redundant=n_redu, random_state=3)
+
+    X, y = make_blobs(n_samples=len(df.index), centers=3, n_features=df.shape[1] -1, cluster_std=2, random_state=2)
+
+    # split into train and test
+    n_train = int(len(df.index) * 0.8)
+    trainX, testX = X[:n_train, :], X[n_train:, :]
+    trainy, testy = y[:n_train], y[n_train:]
+
+    # define model
+    model = Sequential()
+    model.add(Dense(25, input_dim=2, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit model
+    history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=500, verbose=0)
+    # evaluate the model
+    _, train_acc = model.evaluate(trainX, trainy, verbose=0)
+    _, test_acc = model.evaluate(testX, testy, verbose=0)
+    print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))'''
+
+    # ---------untested section logistic regression
+
+    # Logistic regression model
+
+
+
+    '''y = df.iloc[:,-1:]
+    X = df
+
+
+    logreg = LogisticRegression(max_iter=100000)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, test_size=0.3, random_state=100)
+
+    rfe = RFE(logreg)  # running RFE with 15 variables as output
+    rfe = rfe.fit(X_train, y_train)
+
+    list(zip(X_train.columns, rfe.support_, rfe.ranking_))
+
+    col = X_train.columns[rfe.support_]
+
+    X_train_sm = sm.add_constant(X_train[col])
+    logm2 = sm.GLM(y_train, X_train_sm, family=sm.families.Binomial())
+    res = logm2.fit()
+    res.summary()'''
+
+
+
+
+
+
+
+
+    #-----------
 
 
 
@@ -195,10 +264,10 @@ def createGraph (general, df1, df2): #3rd point
 # Main
 if __name__ == '__main__':
 
-    nr_repetitions = 5
+    nr_repetitions = 10
     acuracy_avr = 0
 
-    for i in range(0, 5):
+    for i in range(0, nr_repetitions):
 
         generalDataFrame, avr = readFile("spambase.data")
 
